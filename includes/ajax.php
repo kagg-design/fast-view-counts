5a9c51636f2a1b70144329435fd2c352431424bc
kagg-design <info@kagg.eu>
2020-02-28 21:47:35 +0200

    Fix bug with $_SERVER['DOCUMENT_ROOT'].
    Some servers return path with trailing slash.

--- includes/ajax.php
+++ includes/ajax.php
@@ -6,7 +6,8 @@ define( 'SHORTINIT', true );
 
 // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.MissingUnslash
 $root = isset( $_SERVER['DOCUMENT_ROOT'] ) ? filter_var( $_SERVER['DOCUMENT_ROOT'], FILTER_SANITIZE_STRING ) : '';
-require_once $root . 'wp-load.php';
+$root = rtrim( $root, '/\\' );
+require_once $root . '/wp-load.php';
 
 //require_once $root . '/wp-includes/pluggable.php';
 //require_once $root . '/wp-includes/user.php';
