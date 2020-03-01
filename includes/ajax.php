50dc85d691269d76fbceb663f5e20f9f2eef90de
kagg-design <info@kagg.eu>
2020-03-01 20:36:59 +0200

    Add setting of locale to format number of views properly.

--- includes/ajax.php
+++ includes/ajax.php
@@ -11,6 +11,16 @@ $root = isset( $_SERVER['DOCUMENT_ROOT'] ) ? filter_var( $_SERVER['DOCUMENT_ROOT
 $root = rtrim( $root, '/\\' );
 require $root . '/wp-load.php';
 
+// Components needed for number_format_i18n() to work properly.
+require $root . '/wp-includes/l10n.php';
+require $root . '/wp-includes/class-wp-locale.php';
+
+load_default_textdomain();
+// phpcs:disable WordPress.WP.GlobalVariablesOverride.Prohibited
+$GLOBALS['locale']    = get_locale();
+$GLOBALS['wp_locale'] = new WP_Locale();
+// phpcs:enable WordPress.WP.GlobalVariablesOverride.Prohibited
+
 // Here are the core components needed for check_ajax_referer() to work.
 require $root . '/wp-includes/capabilities.php';
 require $root . '/wp-includes/class-wp-roles.php';
@@ -24,15 +34,13 @@ require $root . '/wp-includes/rest-api.php';
 
 wp_plugin_directory_constants();
 wp_cookie_constants();
-// End of core components needed for check_ajax_referer() to work.
 
 require $root . '/wp-includes/pluggable.php';
 
+// Load main plugin class.
 require_once '../classes/class-main.php';
 
 if ( ! isset( $fast_view_counts ) ) {
 	$fast_view_counts = new Main();
 	$fast_view_counts->update_view_counts();
 }
-
-
